4fdcc7ec44bf812f4785214ff3d45624e5dd5094
Warahi Spenceley <wizzzo@protonmail.com>
2021-03-18 12:35:59 +1300

    tweak table data

--- client/components/WaimaramaBeach.jsx
+++ client/components/WaimaramaBeach.jsx
@@ -11,52 +11,81 @@ function WaimaramaBeach ({ dispatch, waimarama }) {
     <>
       <h1>Waimarama Beach</h1>
       <table>
-          {waimarama.map(day => (
-            <>
+        {waimarama.map(day => (
+          <>
             <tr>
-            <th>{day.date}</th>
+              <th>{day.date}</th>
             </tr>
-              {day.hourly.map(hour => (
+
+            {day.astronomy.map(astro => (
+              <>
+                <tr>
+                  <th>Sunrise</th>
+                  <th>Sunset</th>
+                </tr>
+                <tr>
+                  <td>{astro.sunrise}</td>
+                  <td>{astro.sunset}</td>
+                </tr>
+              </>
+            ))}
+
+            {day.tides.map(tides => (
+              tides.tide_data.map(tidesData => (
                 <>
                   <tr>
-                    <th>Time</th>
-                    <th>swell</th>
+                    <th>Tides</th>
                     <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th>Wind</th>
-                    <th></th>
-                    <th></th>
-                    <th>Gust</th>
-                    <th>{hour.weatherDesc.map(desc => (
-                    <td>{desc.value}</td>
-                  ))}</th>
                   </tr>
                   <tr>
-                    <td>{hour.time}</td>
-                    <td>{hour.swellHeight_ft} ft</td>
-                    <td>{hour.swellDir16Point}</td>
-                    <td>{hour.swellDir}&#176;</td>
-                    <td>{hour.swellPeriod_secs}s</td>
-                    <td>{hour.sigHeight_m}m</td>
+                    <td>{tidesData.tideTime}</td>
+                    <td>{tidesData.tide_type}</td>
+                  </tr>
+                </>
+              ))
+            ))}
 
-                    <td>{hour.winddir16Point}</td>
-                    <td>{hour.winddirDegree}&#176;</td>
-                    <td>{hour.windspeedKmph} k/ph</td>
-                    <td>{hour.WindGustKmph} k/ph</td>
+            {day.hourly.map(hour => (
+              <>
+                <tr>
+                  <th>Time</th>
+                  <th>Swell</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th>Wind</th>
+                  <th></th>
+                  <th></th>
+
+                  <th>temp&#176;</th>
+                  <th>water&#176;</th>
+
+                </tr>
+                <tr>
+                  <td>{hour.time}</td>
+                  <td>{hour.swellHeight_ft} ft</td>
+                  <td>{hour.swellDir16Point}</td>
+                  <td>{hour.swellDir}&#176;</td>
+                  <td>{hour.swellPeriod_secs}s</td>
+
+                  <td>{hour.winddir16Point}</td>
+                  <td>{hour.winddirDegree}&#176;</td>
+                  <td>{hour.windspeedKmph}k/ph</td>
+
+                  <td>{hour.tempC}&#176;</td>
+                  <td>{hour.waterTemp_C}&#176;</td>
 
                   {hour.weatherIconUrl.map(icon => (
                     <td>
                       <img src={icon.value} alt="weather icon"></img>
-                    </td> 
+                    </td>
                   ))}
-                  </tr>
-                </>
-              ))}
-            </>
-          ))}
-    
+                </tr>
+              </>
+            ))}
+          </>
+        ))}
+
       </table>
     </>
   )
